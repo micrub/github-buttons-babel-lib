6119f3a08d8eb34687dd63e1ecdc1af23ef1e9e8
Michael Rubanov <mrubanov@gmail.com>
2017-07-20 21:48:15 +0300

    Squashed commit of the following:
    Author: Michael Rubanov <mrubanov@gmail.com>
    Date:   Thu Jul 20 21:27:08 2017 +0300
    
        * Node modules update.
        * `package.json` tempalte changes.
        * Cleanup.

---
+++ test/BaseSpec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import Base from '../src/Base';
+
+describe('Base', () => {
+
+  describe('Base', () => {
+
+    it('should be instance of object', () => {
+      expect(Base).to.be.instanceOf(Object)
+    });
+
+  });
+
+});
