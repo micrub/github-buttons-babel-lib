94b07e2faa089d6608dc24be5113773f0acad06e
Michael Rubanov <mrubanov@gmail.com>
2017-07-20 22:32:06 +0300

    Browserify @dev-dep , browser usage guard stub

--- src/Base.js
+++ src/Base.js
@@ -1,5 +1,9 @@
 class Base {
-  constructor() {}
+  constructor() {
+    if (typeof window === "undefined") {
+      throw new Error("Build to run only in browser environment.")
+    }
+  }
 }
 
 export default Base;
