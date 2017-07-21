1c1ca35ff1feeb28c122094d4d6d9d40cf418fd3
Michael Rubanov <mrubanov@gmail.com>
2017-07-22 00:18:29 +0300

    remove eslint plugin, from airbnb from config.

--- src/Base.js
+++ src/Base.js
@@ -1,34 +1,34 @@
-import Renderer from "./Renderer";
-import GitHubButton from "./React/Component/GitHubButton";
+/* global define */
+/* eslint no-undef: "error" */
+
+import Renderer from './Renderer';
+import GitHubButton from './React/Component/GitHubButton';
 
 class Base {
+  initiated = false;
   constructor() {
-    if (typeof window === "undefined") {
-      throw new Error("Build to run only in browser environment.");
+    if (typeof window === 'undefined') {
+      throw new Error('Build to run only in browser environment.');
     } else {
       this.init();
     }
   }
   init() {
-    let self = this;
-    if (typeof define === "function" && define.amd) {
-
+    if (typeof define === 'function' && define.amd) {
       /**
        * Export render and GitHubButton React componenet in case of AMD
        */
-
       define([], { render: Renderer.render, GitHubButton });
-
     } else {
       if (
-        !{}.hasOwnProperty.call(document, "currentScript") &&
+        !{}.hasOwnProperty.call(document, 'currentScript') &&
           document.currentScript &&
           delete document.currentScript &&
           document.currentScript
       ) {
         Renderer.BASEURL = document.currentScript.src.replace(
-          /[^\/]*([?#].*)?$/,
-          ""
+          /[^/]*([?#].*)?$/,
+          '',
         );
       }
       if (document.title === Renderer.UUID) {
@@ -36,6 +36,7 @@ class Base {
       } else {
         Renderer.defer(Renderer.renderAll);
       }
+      this.initiated = true;
     }
   }
 }
